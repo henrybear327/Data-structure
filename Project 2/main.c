45df2a8a5f7bf70925e3ad586e49518ed82431af
Chun-Hung Tseng <henrybear327@gmail.com>
2015-11-04 20:48:52 +0800

    Started but delete function is not finish yet

--- Project 2/main.c
+++ Project 2/main.c
@@ -131,6 +131,20 @@ void BST_inorder_terversal(Node *curr)
     BST_inorder_terversal(curr->right);
 }
 
+void BST_delete()
+{
+    printf("Which number do you want to delete? ");
+    int inp;
+    scanf("%d", &inp);
+
+    Node *to_delete = BST_search(BST_start_ptr, inp);
+    if (to_delete == NULL) {
+        printf("The number %d doesn't exist!\n", inp);
+        return;
+    }
+
+}
+
 /*
 Prints the menu for BST homework.
 Also, calls the appropriate functions to do the demanded jobs.
@@ -160,6 +174,7 @@ void binary_search_tree()
             BST_insert();
         } else if (choice == 'D' || choice == 'd') {
             // delete
+            BST_delete();
         } else if (choice == 'S' || choice == 's') {
             // search
             printf("Which number do you want to search? ");
@@ -188,9 +203,7 @@ int main()
 
     while (1) {
         char choice = welcome_msg();
-#if DEBUG
-        printf("input = %c\n", choice);
-#endif
+
         if (choice == 'B' || choice == 'b') {
             binary_search_tree();
         } else if (choice == 'T' || choice == 't') {
