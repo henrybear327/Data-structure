d75ba794854b111fe273901018718102a5a7b28a
Chun-Hung Tseng <henrybear327@gmail.com>
2015-11-10 08:15:03 +0800

    Add delete to BST

--- Random Code/BST/main.cpp
+++ Random Code/BST/main.cpp
@@ -1,4 +1,5 @@
-// C program to demonstrate insert operation in binary search tree
+// C program to demonstrate delete operation in binary search tree
+// http://geeksquiz.com/binary-search-tree-set-2-delete/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,7 +22,7 @@ void inorder(struct node *root)
 {
     if (root != NULL) {
         inorder(root->left);
-        printf("%d \n", root->key);
+        printf("%d ", root->key);
         inorder(root->right);
     }
 }
@@ -36,14 +37,73 @@ struct node *insert(struct node *node, int key)
     /* Otherwise, recur down the tree */
     if (key < node->key)
         node->left = insert(node->left, key);
-    else if (key > node->key)
+    else
         node->right = insert(node->right, key);
 
     /* return the (unchanged) node pointer */
     return node;
 }
 
-/*A utility function to copy a BST and returns the root addr.*/
+/* Given a non-empty binary search tree, return the node with minimum
+key value found in that tree. Note that the entire tree does not
+need to be searched. */
+struct node *minValueNode(struct node *node)
+{
+    struct node *current = node;
+
+    /* loop down to find the leftmost leaf */
+    while (current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+/* Given a binary search tree and a key, this function deletes the key
+and returns the new root */
+struct node *deleteNode(struct node *root, int key)
+{
+    // base case
+    if (root == NULL)
+        return root;
+
+    // If the key to be deleted is smaller than the root's key,
+    // then it lies in left subtree
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+
+    // If the key to be deleted is greater than the root's key,
+    // then it lies in right subtree
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+
+    // if key is same as root's key, then This is the node
+    // to be deleted
+    else {
+        // node with only one child or no child
+        if (root->left == NULL) {
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        // node with two children: Get the inorder successor (smallest
+        // in the right subtree)
+        struct node *temp = minValueNode(root->right);
+
+        // Copy the inorder successor's content to this node
+        root->key = temp->key;
+
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
+
+/*An utility function to copy a BST and returns the root addr.*/
 struct node *copy_BST(struct node *curr)
 {
     if (curr == NULL)
@@ -59,27 +119,40 @@ struct node *copy_BST(struct node *curr)
 // Driver Program to test above functions
 int main()
 {
-    /* Let us create following BST
-                  50
-                             /     \
-                                       30      70
-                                                /  \    /  \
-                                                       20   40  60   80 */
+    /*
+        Let us create following BST
+               50
+            /	  \
+           30	  70
+           / \   / \
+          20 40 60 80
+    */
     struct node *root = NULL;
     root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 20);
-    insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
 
-    // print inoder traversal of the BST
+    printf("Inorder traversal of the given tree \n");
     inorder(root);
-    printf("=====\n");
 
-    struct node *copy_head = copy_BST(root);
-    inorder(copy_head);
+    printf("\nDelete 20\n");
+    root = deleteNode(root, 20);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+
+    printf("\nDelete 30\n");
+    root = deleteNode(root, 30);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+
+    printf("\nDelete 50\n");
+    root = deleteNode(root, 50);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
 
     return 0;
 }
