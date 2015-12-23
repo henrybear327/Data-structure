8aee6f89c70083326efce80635dfd02641d9eef7
Chun-Hung Tseng <henrybear327@gmail.com>
2015-12-23 14:10:13 +0800

    Add q_sort function

--- Project 4/main.c
+++ Project 4/main.c
@@ -36,6 +36,59 @@ bool show_column[6];
 #define PASS_CHECKS 1
 #define QUIT 2
 
+int cmp(const void *a, const void *b)
+{
+    return *(int *)a - *(int *)b;
+}
+
+void q_sort(void *mem_start, size_t total_member, size_t member_size,
+            int (*cmp)(const void *, const void *))
+{
+    // zd for printing size_t
+    // printf("arguments : %p, %zd, %zd, %p\n", mem_start, total_member,
+    // member_size, cmp);
+    if (total_member > 1) {
+        void *pivot = mem_start;
+        void *to_compare = mem_start + (total_member - 1) * member_size;
+
+        while (pivot != to_compare) {
+            if ((cmp(pivot, to_compare) > 0 && to_compare > pivot) ||
+                (cmp(to_compare, pivot) > 0 &&
+                 pivot > to_compare)) { // pivot > to_compare
+                // pivot <-> to_compare
+                void *temp = malloc(member_size);
+                memcpy(temp, pivot, member_size);
+                memcpy(pivot, to_compare, member_size);
+                memcpy(to_compare, temp, member_size);
+                free(temp);
+
+                temp = pivot;
+                pivot = to_compare;
+                to_compare = temp;
+
+                if (pivot < to_compare)
+                    to_compare -= member_size;
+                else
+                    to_compare += member_size;
+            } else {
+                if (pivot < to_compare)
+                    to_compare -= member_size;
+                else
+                    to_compare += member_size;
+            }
+        }
+
+        q_sort(mem_start, ((pivot - mem_start) / member_size), member_size, cmp);
+
+        q_sort(pivot + member_size,
+               (total_member - ((to_compare - mem_start) / member_size + 1)),
+               member_size, cmp);
+    } else {
+        // only one or less element, no need to sort
+        return;
+    }
+}
+
 /*
 Remove \n\r from the string
 */
